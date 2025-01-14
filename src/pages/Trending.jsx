0542a06824b1b70d9c792c6f243ece20d0024dd7
Mohamed Riyas <mohamedriyas.dev@gmail.com>
2025-01-14 11:34:58 +0530

    Update Trending.jsx

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../styles/LatestNews.css';
+import '../styles/latestNews.css';
 import LatestNews from '../components/LatestNews';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
